53ae8b4451c78cb225e03bfcf9ae8d73a9b38075
Cameron Alsbrook <CameronAlsbrook@gmail.com>
2018-02-08 19:11:00 -0800

    heroku deploy

--- controllers/leaderboardController.js
+++ controllers/leaderboardController.js
@@ -1,6 +1,7 @@
 var request =require('request');
 var Leaderboard = require('../models/leaderboard')
 
+
 function leaderboardDetails(req, res) {
     res.render('leaderboard', {title: 'Leaderboard'})
 }
